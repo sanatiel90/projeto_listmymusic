86191a27e9cd80ce500b4112ce8f49c0110db3a9
sanatiel90 <sanatielbarros@gmail.com>
2017-03-02 10:18:17 -0300

    Added play link to pages with ajax

--- myscript.js
+++ myscript.js
@@ -28,7 +28,7 @@ $(document).ready(function(){
 				table += '<td class="text-center">'+e[key].name_band+'</td>';
 				table += '<td class="text-center">'+e[key].name_category+'</td>'; 
 				table += '<td class="text-center">'+e[key].comment_music+'</td>';
-				table += '<td class="text-center"><a href="edit_music.php?id='+e[key].id_music+'" class="btn btn-md btn-primary"><span class="glyphicon glyphicon-edit"></span></a>   <button data-toggle="modal" data-target="#delete-music" value="'+e[key].id_music+'"  type="button" class="bt-delete btn btn-md btn-danger"><span class="glyphicon glyphicon-trash"></span></button></td>';
+				table += '<td class="text-center"><a href="edit_music.php?id='+e[key].id_music+'" class="btn btn-md btn-primary"><span class="glyphicon glyphicon-edit"></span></a>   <button data-toggle="modal" data-target="#delete-music" value="'+e[key].id_music+'"  type="button" class="bt-delete btn btn-md btn-danger"><span class="glyphicon glyphicon-trash"></span></button> <a href="https://www.youtube.com/results?search_query='+e[key].name_band+'+'+e[key].name_music+'" target="_blank" class="btn btn-md btn-default" style="background-color:#9932CC; color:white"><span class="glyphicon glyphicon-play"></span></a> </td>';
 				table += '</tr>';
 			}
 
@@ -73,7 +73,7 @@ $('#ordenation-wait').change(function(){
 				table += '<td class="text-center">'+e[key].name_band+'</td>';
 				table += '<td class="text-center">'+e[key].name_category+'</td>'; 
 				table += '<td class="text-center">'+e[key].comment_music+'</td>';
-				table += '<td class="text-center"><a href="controller/control_change_level.php?id='+e[key].id_music+'" class="btn btn-md btn-warning"><span class="glyphicon glyphicon-cd"></span></a>  <a href="edit_music.php?id='+e[key].id_music+'" class="btn btn-md btn-primary"><span class="glyphicon glyphicon-edit"></span></a>   <button data-toggle="modal" data-target="#delete-music" value="'+e[key].id_music+'"  type="button" class="bt-delete btn btn-md btn-danger"><span class="glyphicon glyphicon-trash"></span></button></td>';
+				table += '<td class="text-center"><a href="controller/control_change_level.php?id='+e[key].id_music+'" class="btn btn-md btn-warning"><span class="glyphicon glyphicon-cd"></span></a>  <a href="edit_music.php?id='+e[key].id_music+'" class="btn btn-md btn-primary"><span class="glyphicon glyphicon-edit"></span></a>   <button data-toggle="modal" data-target="#delete-music" value="'+e[key].id_music+'"  type="button" class="bt-delete btn btn-md btn-danger"><span class="glyphicon glyphicon-trash"></span></button>  <a href="https://www.youtube.com/results?search_query='+e[key].name_band+'+'+e[key].name_music+'" target="_blank" class="btn btn-md btn-default" style="background-color:#9932CC; color:white"><span class="glyphicon glyphicon-play"></span></a> </td>';
 				table += '</tr>';
 			}
 
